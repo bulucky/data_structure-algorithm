3d029aedfa2bb1b01dc326e3918812fc49a6f860
Hizamer <848863325@qq.com>
2025-10-24 14:59:19 +0800

    recursion exercise

--- src/recursion/recursion.c
+++ src/recursion/recursion.c
@@ -27,12 +27,49 @@ int stack_sim_recurson(int n) {
     return res;
 }
 
+int fibonacci(int n) {
+    if(n == 0) return 0;
+    else if(n == 1) return 1;
+
+    int res = (fibonacci(n-1) + fibonacci(n-2));
+
+    return res;
+}
+
+/**
+*@brief 快速计算幂
+*
+*@param int a, int n    a^n
+*
+*@return size_t         结果
+*/ 
+size_t quick_pow(int a, int n) {
+    int res = 1;
+    if(n == 0) {
+        return 1;
+    }else if(n % 2 == 1) {
+        res = a * quick_pow(a, n-1);
+    }else {
+        int temp = quick_pow(a, n/2);
+        res = temp * temp;
+    }
+
+    return res;
+}
+
 int main(int argc, char const* argv[])
 {
-    int sums = sum(9);
-    printf("%d \n", sums);
-    int sim_sums = stack_sim_recurson(9);
-    printf("%d \n", sim_sums);
+    // int sums = sum(9);
+    // printf("%d \n", sums);
+    // int sim_sums = stack_sim_recurson(9);
+    // printf("%d \n", sim_sums);
+
+    
+    // int res_fib = fibonacci(0);
+    // printf("%d", res_fib);
+
+    size_t res_q_pow = quick_pow(2, 4);
+    printf("%zu", res_q_pow);
 
     return 0;
 }
