73727c72ed18e2beb5ca454ba8aff45fcd2af50d
Hizamer <848863325@qq.com>
2026-02-14 21:29:52 +0800

    :poop: remove subtree exists

--- src/tree/binary_tree_serach.cpp
+++ src/tree/binary_tree_serach.cpp
@@ -27,10 +27,14 @@ public:
     }
 
     bool bst_find(int val) {
+        is_find = false;
         return find(val, root_);
     }
 
-    void bst_delete(int val) {
+    bool bst_delete(int val) {
+        is_remove = false;
+        pre = root_;
+        return remove(val, root_);
     }
 
     void bst_print(TreeNode* root = nullptr) {
@@ -44,6 +48,9 @@ public:
 
 private:
     bool is_find = false;
+    bool is_remove = false;
+    bool is_left = false;
+    TreeNode* pre = nullptr;
 
     void insert(TreeNode* n_node, TreeNode* t_node) {
         if (n_node->val < t_node->val) {
@@ -75,18 +82,69 @@ private:
         }
         return is_find;
     }
+
+    bool remove(int val, TreeNode* t_node) {
+        if (val == t_node->val) {
+            is_remove = true;
+            if (t_node->left == nullptr && t_node->right == nullptr) {
+                delete t_node;
+                t_node = nullptr;
+                if (is_left) {
+                    pre->left = nullptr;
+                } else {
+                    pre->right = nullptr;
+                }
+            } else if (t_node->left == nullptr || t_node->right == nullptr) {
+                if (t_node->left == nullptr) {
+                    t_node->val = t_node->right->val;
+                    delete t_node->right;
+                    t_node->right = nullptr;
+                } else {
+                    t_node->val = t_node->left->val;
+                    delete t_node->left;
+                    t_node->left = nullptr;
+                }
+            } else {
+                TreeNode* tmp = t_node;
+                tmp->val = val;
+                // remove(val, t_node->right);
+            }
+        } else if (t_node->left != nullptr && val < t_node->val) {
+            pre = t_node;
+            is_left = true;
+            remove(std::forward<int>(val), t_node->left);
+        } else if (t_node->right != nullptr && val > t_node->val) {
+            pre = t_node;
+            is_left = false;
+            remove(std::forward<int>(val), t_node->right);
+        }
+
+        return is_remove;
+    }
 };
 
 int main(int argc, char const* argv[]) {
     TreeNode* root = new TreeNode(-1);
     BinarySerachTree* bst = new BinarySerachTree(root);
 
-    bst->bst_insert(-2);
+    bst->bst_insert(-3);
     bst->bst_insert(3);
+    bst->bst_insert(-6);
+    bst->bst_insert(-5);
+    bst->bst_insert(2);
+    bst->bst_insert(5);
 
     bst->bst_print(bst->root_);
+    std::cout << "\n";
 
     std::cout << "find " << bst->bst_find(4) << "\n";
+    std::cout << "find " << bst->bst_find(-2) << "\n";
+    std::cout << "find " << bst->bst_find(3) << "\n";
+    std::cout << "find " << bst->bst_find(5) << "\n";
+
+    bst->bst_delete(2);
+
+    bst->bst_print(bst->root_);
 
     delete bst;
 
